89a54a914e0d415fa14095c626407ff058cd34d8
***** <kn47ytv9mv@privaterelay.appleid.com>
2026-08-18 08:36:10 -0400

    Removed duplicate ingress.

--- resources.tf
+++ resources.tf
@@ -11,14 +11,6 @@ resource "aws_security_group" "alb" {
   }
 
   ingress {
-    description = "HTTP (redirect)"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     description = "HTTPS"
     from_port   = 443
     to_port     = 443
