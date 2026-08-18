802f479b35839b60130cde7c5a7fda58ff17591a
***** <kn47ytv9mv@privaterelay.appleid.com>
2026-08-18 08:45:50 -0400

    Maintain HTTP redirect.

--- resources.tf
+++ resources.tf
@@ -260,17 +260,6 @@ resource "aws_lb_target_group" "jenkins" {
   }
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.jenkins.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.jenkins.arn
-  }
-}
-
 resource "aws_lb_listener" "http_redirect" {
   load_balancer_arn = aws_lb.jenkins.arn
   port              = 80
